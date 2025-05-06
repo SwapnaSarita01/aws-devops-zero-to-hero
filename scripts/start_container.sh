8966358999ff6ba6a6c1473de9a00f99079f094a
SwapnaSarita01 <87479827+SwapnaSarita01@users.noreply.github.com>
2025-05-06 14:48:23 +0530

    add commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 
 # Pull the Docker image from Docker Hub
-docker pull swapna2001/simple-python-app
+docker pull swapna2001/simple-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 swapna2001/simple-python-app
+docker run -d -p 5000:5000 swapna2001/simple-app
