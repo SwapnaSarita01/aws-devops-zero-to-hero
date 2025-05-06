7ff48f6ca452f1024e3fc8ddcbb35a364f951f31
SwapnaSarita01 <87479827+SwapnaSarita01@users.noreply.github.com>
2025-05-06 16:16:29 +0530

    0000

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,10 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+
+container_id=$(docker ps -q)
+if [ -n "$container_id" ]; then
+  docker rm -f $container_id
+else
+  echo "No running containers to remove."
+fi
